e5a30bc12c8ab9b94c4b17ee4379f45ac47a1ec6
Firas Hamdi <firas.hamdi@ml-pa.com>
2023-03-27 17:12:29 +0200

    fixup! fixup! fixup! fixup! fixup! test/can_samd5x: test CAN controller driver

--- tests/can_samd5x/main.c
+++ tests/can_samd5x/main.c
@@ -28,62 +28,40 @@
 
 #include "can_params.h"
 #include "periph/can.h"
+#include "ztimer.h"
 
 static can_t can_samd5x;
+candev_t *candev = NULL;
 
 #define ENABLE_DEBUG 0
 #include <debug.h>
 
-#ifndef TEST_MODE
-#define TEST_MODE     0
-#endif
-
-static candev_t *candev = NULL;
-
 static void _can_event_callback(candev_t *dev, candev_event_t event, void *arg)
 {
-    (void)arg;
     (void)dev;
     struct can_frame *frame;
 
     switch (event) {
     case CANDEV_EVENT_ISR:
         DEBUG("_can_event: CANDEV_EVENT_ISR\n");
-        // dev->driver->isr(candev);
+        dev->driver->isr(candev);
         break;
     case CANDEV_EVENT_WAKE_UP:
         DEBUG("_can_event: CANDEV_EVENT_WAKE_UP\n");
         break;
     case CANDEV_EVENT_TX_CONFIRMATION:
         DEBUG("_can_event: CANDEV_EVENT_TX_CONFIRMATION\n");
+        frame = (struct can_frame *)arg;
+        DEBUG("frame to send ID = 0x%08lx\n", frame->can_id);
         break;
     case CANDEV_EVENT_TX_ERROR:
         DEBUG("_can_event: CANDEV_EVENT_TX_ERROR\n");
         break;
     case CANDEV_EVENT_RX_INDICATION:
         DEBUG("_can_event: CANDEV_EVENT_RX_INDICATION\n");
-
-        frame = (struct can_frame *)arg;
-
-        DEBUG("            id: %" PRIx32 " dlc: %" PRIx8 " data: ", frame->can_id & 0x1FFFFFFF,
-              frame->can_dlc);
-        for (uint8_t i = 0; i < frame->can_dlc; i++) {
-            DEBUG("0x%X ", frame->data[i]);
-        }
-        DEBUG_PUTS("");
-
         break;
     case CANDEV_EVENT_RX_ERROR:
         DEBUG("_can_event: CANDEV_EVENT_RX_ERROR\n");
-        break;
-    case CANDEV_EVENT_BUS_OFF:
-        // dev->state = CAN_STATE_BUS_OFF;
-        break;
-    case CANDEV_EVENT_ERROR_PASSIVE:
-        // dev->state = CAN_STATE_ERROR_PASSIVE;
-        break;
-    case CANDEV_EVENT_ERROR_WARNING:
-        // dev->state = CAN_STATE_ERROR_WARNING;
         break;
     default:
         DEBUG("_can_event: unknown event\n");
@@ -95,33 +73,19 @@ int main(void)
 {
     puts("candev test application\n");
 
+    gpio_init(GPIO_PIN(PC, 13), GPIO_IN);
+    DEBUG("stby = %d\n", gpio_read(GPIO_PIN(PC, 13)));
     can_init(&can_samd5x, &candev_conf[1]);
 
+    candev = &(can_samd5x.candev);
     candev->event_callback = _can_event_callback;
     candev->isr_arg = NULL;
-    candev = &(can_samd5x.candev);
 
     candev->driver->init(candev);
-
-    struct can_frame frame = {
-        .can_dlc = 5,
-        .can_id = 0x8C122330,
-        .data = {
-            0,
-            1,
-            2,
-            3,
-            0xF
-        }
-    };
-
-#if IS_ACTIVE(TEST_MODE)
-    candev_samd5x_enter_test_mode(candev);
-#endif
-
-    candev->driver->send(candev, &frame);
-    candev->driver->send(candev, &frame);
-    candev->driver->send(candev, &frame);
+    candev_samd5x_irq_init(candev, CAN_TSW_IRQ, CAN_IRQ_LINE_1);
+    candev_samd5x_irq_init(candev, CAN_TEFN_IRQ, CAN_IRQ_LINE_1);
+    candev_samd5x_irq_init(candev, CAN_TFE_IRQ, CAN_IRQ_LINE_1);
+    candev_samd5x_irq_init(candev, CAN_DRX_IRQ, CAN_IRQ_LINE_1);
 
     struct can_filter filter = {0};
     filter.can_filter_conf = CAN_FILTER_RX_FIFO_1;
@@ -141,10 +105,41 @@ int main(void)
     filter.can_id = 0x8C000001;
     filter.can_mask = 0x9FFFFFFF;
     candev->driver->set_filter(candev, &filter);
-    
+
     filter.can_id = 0x8C000001;
     filter.can_mask = 0x9FFFFFFF;
     candev->driver->remove_filter(candev, &filter);
+
+    struct can_frame frame_1 = {
+        .can_dlc = 5,
+        .can_id = 0x8C122330,
+        .data = {
+            0,
+            1,
+            2,
+            3,
+            0xF
+        }
+    };
+
+        struct can_frame frame_2 = {
+        .can_dlc = 5,
+        .can_id = 0x9B001122,
+        .data = {
+            4,
+            5,
+            6,
+            7,
+            0xE
+        }
+    };
+
+#if IS_ACTIVE(TEST_MODE)
+    candev_samd5x_enter_test_mode(candev);
+#endif
+
+    candev->driver->send(candev, &frame_1);
+    candev->driver->send(candev, &frame_2);
 
     return 0;
 }
